4f89b0c529141304119975f14f2c097ec9d91b6d
emmistasegasubama <74078715+emmistasegasubama@users.noreply.github.com>
2020-11-10 10:34:59 +0700

    Add files via upload

---
+++ Soal-2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+R = int(input("Jari-Jari Lingkaran"))
+L = (22/7 * R * R)
+print("Luas Lingkaran dengan jari-jari", R, "cm", "adalah", L, "cm\u00b2")
+
+
+# In[ ]:
+
+
+
+
