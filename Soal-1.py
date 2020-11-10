c84bfb7c2eefc8b686e9596571ea9407e34a7a31
emmistasegasubama <74078715+emmistasegasubama@users.noreply.github.com>
2020-11-10 10:17:59 +0700

    Add files via upload

---
+++ Soal-1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+Nama = "Cak Bidin"
+Umur = 43
+Tinggi =185
+print("Nama Saya", Nama, "Umur saya", Umur, "tahun", "dan tinggi saya", Tinggi, "cm")
+
+
+# In[ ]:
+
+
+
+
